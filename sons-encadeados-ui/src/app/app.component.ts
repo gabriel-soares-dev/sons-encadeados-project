238d1fafadeb8a4e7d22eb62e9da48de3b72d04b
Gabr1el94 <bielsystem64@gmail.com>
2024-07-15 21:13:10 -0300

    rename prefix and test new component

--- sons-encadeados-ui/src/app/app.component.ts
+++ sons-encadeados-ui/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sons-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sons-encadeados-ui';
+  title = 'testando sons-encadeados-ui ...';
 }
