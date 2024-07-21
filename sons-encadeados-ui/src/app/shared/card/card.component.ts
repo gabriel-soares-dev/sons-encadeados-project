1ff1c8a77d7f982bdeadecc82d1ba6a485316eef
Gabr1el94 <bielsystem64@gmail.com>
2024-07-21 11:57:49 -0300

    add card and button next on  component home

---
+++ sons-encadeados-ui/src/app/shared/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sons-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
--- sons-encadeados-ui/src/app/shared/shared.module.ts
+++ sons-encadeados-ui/src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, InputComponent
+    CommonModule, InputComponent, CardComponent
   ],
-  exports: [CommonModule, InputComponent]
+  exports: [CommonModule, InputComponent, CardComponent]
 })
 export class SharedModule { }
