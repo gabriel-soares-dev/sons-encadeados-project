cc5b0d3ada822337402c7a164e930556e485ddf1
Gabr1el94 <bielsystem64@gmail.com>
2024-07-21 22:15:58 -0300

    news components in the project

---
+++ sons-encadeados-ui/src/app/detail/detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sons-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.css'
+})
+export class DetailComponent {
+
+}
---
+++ sons-encadeados-ui/src/app/summary/summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sons-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './summary.component.html',
+  styleUrl: './summary.component.css'
+})
+export class SummaryComponent {
+
+}
